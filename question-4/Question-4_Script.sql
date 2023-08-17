b4359301540e53ca7f3d55d922dc3bbd20303963
Iury de Mattos <iurydemattos@gmail.com>
2023-08-17 20:07:07 -0300

    DAY 3

--- question-4/Question-4_Script.sql
+++ question-4/Question-4_Script.sql
@@ -18,11 +18,33 @@ CREATE TABLE Address (
 CREATE TABLE Person (
   person_id SERIAL PRIMARY Key ,
   name VARCHAR(64) NOT NULL,
-  age INTEGER ,
+  birthday DATE NOT NULL ,
+  age INTERVAL ,
   phone VARCHAR(16) ,
-  height DECIMAL(1,2) ,
+  height DECIMAL(4,2) ,
   email VARCHAR(128) NOT NULL ,
   cpf VARCHAR(11) NOT NULL ,
-  birthday DATE NOT NULL ,
   address_id INTEGER REFERENCES Address(addressId)
+);
+
+INSERT INTO address (zip, street, complement, neighborhood, city, state, country)
+VALUES (
+  '840050190' ,
+  'RUA PARANAGUA, 5' ,
+  'BLOCO 1, AP32' ,
+  'PONTA GROSSA' ,
+  'VILA ESTRELA' ,
+  'PARANA' , 
+  'BRAZIL' );
+
+INSERT INTO person (name, birthday, age, phone, height, email, cpf, address_id)
+VALUES (
+  'IURY DE MATTOS' ,
+  '1995-11-07' ,
+  AGE(TIMESTAMP '1995-11-07') ,
+  '5542999254823' ,
+  1.83,
+  'IURY.MATTOS.PB@COMPASS.COM.BR' , 
+  '09834667930' ,
+  1
 );
