107f025c24e8781bd050d5c360b65084dff30396
Iury de Mattos <iurydemattos@gmail.com>
2023-08-19 00:14:35 -0300

    DAY 5

--- question-3/README.md
+++ question-3/README.md
@@ -20,3 +20,113 @@
         Test 2
         result
 
+
+
+//version 1
+package src;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      try {
+        String m = scanner.nextLine();
+        int M = Integer.parseInt(m);
+        if (M == 0) break;
+        else if (!(M < 0) && !(M > 100)) {
+          String op = scanner.nextLine();
+          if(op.charAt(0)=='-') M++;
+          op = op.replaceAll(" ", "");
+          String str = op.replaceAll("[0-9]", "");
+          if (M == str.length()) {
+            str = op;
+            str = str.replaceAll("[+]", " ");
+            str = str.replaceAll("-", " -").trim();
+
+            String[] numbers = str.split(" ");
+            int plusCounter =0;
+            int minusCounter =0;
+            for (String n : numbers) {
+              int num = Integer.parseInt(n);
+              if (num >= 0) {
+                plusCounter++;
+              }
+              else{
+                minusCounter++;
+              }
+            }
+            int[] positive = new int[plusCounter];
+            int[] negative = new int[minusCounter];
+            int index = 0;
+            for (String n : numbers
+            ) {
+              int num = Integer.parseInt(n);
+              if (num >= 0) {
+                positive[index] = num;
+                index++;
+              }
+            }
+            index = 0;
+
+            for (String n : numbers
+            ) {
+              int num = Integer.parseInt(n);
+              if (num < 0) {
+                negative[index] = num;
+                index++;
+              }
+            }
+            System.out.println("POSITIVES");
+            for(int i : positive){
+              System.out.print(i + " ");
+            }
+            System.out.println("\nNEGATIVES");
+            for(int i : negative){
+              System.out.print(i + " ");
+            }
+          }
+        }
+
+
+        //System.out.println("STR:  "+ str);
+        //String[] operators = numbers.split("");
+        // String[] operands = op.split("[+-]");
+        // for (String o: operators
+        // ) {
+        //   System.out.println(o);
+        //  }
+
+        //OK
+        //op = op.replaceAll(" ","");
+        //
+        //int i =0;
+        //int counter = 0;
+        //while(true){
+        //  int founder = op.indexOf("-", i);
+        //  if (founder == -1)break;
+        //  counter++;
+        //  i = founder+1;
+        // }
+        // System.out.println(counter);
+        // op = op.replaceAll("[+]"," ");
+        // System.out.println(op);
+
+
+      } catch (Exception e) {
+
+      }
+    }
+  }
+}
+/*
+      String op = scanner.nextLine();
+      if(!op.isEmpty()){
+        Calculator.Calculate(op,m);
+      }
+    }
+
+ */
--- question-3/src/src/Main.java
+++ question-3/src/src/Main.java
@@ -1,3 +1,8 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -12,42 +17,86 @@ public class Main {
         if (M == 0) break;
         else if (!(M < 0) && !(M > 100)) {
           String op = scanner.nextLine();
+          if(op.charAt(0)=='-') M++;
           op = op.replaceAll(" ", "");
           String str = op.replaceAll("[0-9]", "");
           if (M == str.length()) {
-          str = op;
-          System.out.println("STR:  "+ str);
-          }
-          //String[] operators = numbers.split("");
-          // String[] operands = op.split("[+-]");
-          // for (String o: operators
-          // ) {
-          //   System.out.println(o);
-          //  }
-          //for (String p : operands
-          // ) {
-          //   System.out.println(p);
-          // }
-          //OK
-          //op = op.replaceAll(" ","");
-          //
-          //int i =0;
-          //int counter = 0;
-          //while(true){
-          //  int founder = op.indexOf("-", i);
-          //  if (founder == -1)break;
-          //  counter++;
-          //  i = founder+1;
-          // }
-          // System.out.println(counter);
-          // op = op.replaceAll("[+]"," ");
-          // System.out.println(op);
+            str = op;
+            str = str.replaceAll("[+]", " ");
+            str = str.replaceAll("-", " -").trim();
 
+            String[] numbers = str.split(" ");
+            int plusCounter =0;
+            int minusCounter =0;
+            for (String n : numbers) {
+              int num = Integer.parseInt(n);
+              if (num >= 0) {
+                plusCounter++;
+              }
+              else{
+                minusCounter++;
+              }
+            }
+            int[] positive = new int[plusCounter];
+            int[] negative = new int[minusCounter];
+            int index = 0;
+            for (String n : numbers
+            ) {
+              int num = Integer.parseInt(n);
+              if (num >= 0) {
+                positive[index] = num;
+                index++;
+              }
+            }
+            index = 0;
+
+            for (String n : numbers
+            ) {
+              int num = Integer.parseInt(n);
+              if (num < 0) {
+                negative[index] = num;
+                index++;
+              }
+            }
+            int result =
+                    Arrays.stream(positive).sum() +
+                            Arrays.stream(negative).sum();
+
+            System.out.println(result);
+          }
         }
+
+
+        //System.out.println("STR:  "+ str);
+        //String[] operators = numbers.split("");
+        // String[] operands = op.split("[+-]");
+        // for (String o: operators
+        // ) {
+        //   System.out.println(o);
+        //  }
+
+        //OK
+        //op = op.replaceAll(" ","");
+        //
+        //int i =0;
+        //int counter = 0;
+        //while(true){
+        //  int founder = op.indexOf("-", i);
+        //  if (founder == -1)break;
+        //  counter++;
+        //  i = founder+1;
+        // }
+        // System.out.println(counter);
+        // op = op.replaceAll("[+]"," ");
+        // System.out.println(op);
+
+
       } catch (Exception e) {
 
       }
-
+    }
+  }
+}
 /*
       String op = scanner.nextLine();
       if(!op.isEmpty()){
@@ -56,6 +105,3 @@ public class Main {
     }
 
  */
-    }
-  }
-}
